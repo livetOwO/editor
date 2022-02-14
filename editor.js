39c5891dae780edf9fc49f106b5eef0e025e260e
han-bom <livet@kakao.com>
2022-02-14 21:49:53 +0900

    add new line function

--- editor.js
+++ editor.js
@@ -2,6 +2,19 @@ function createRow() {
 	const element = document.createElement('div');
 	element.contentEditable = true;
 
+	element.addEventListener('keypress', function (e) {
+		if (!e.shiftKey && e.key === 'Enter') {
+			e.preventDefault();
+			this.newLine();
+		}
+	});
+
+	element.newLine = function () {
+		const line = createRow();
+		this.after(line);
+		line.focus();
+	}
+
 	return element;
 }
 
